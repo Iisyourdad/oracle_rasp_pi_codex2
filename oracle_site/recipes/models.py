fecbadf460fea457587211d88351d444aa9af26d
Iisyourdad <76657591+Iisyourdad@users.noreply.github.com>
2025-11-07 23:53:23 -0600

    Add user-specific homepage backgrounds

--- oracle_site/recipes/models.py
+++ oracle_site/recipes/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class HomePage(models.Model):
     title = models.CharField(max_length=200, default="Westbrook Recipes")
@@ -7,6 +9,25 @@ class HomePage(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    background_image = models.ImageField(upload_to="backgrounds/", blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    UserProfile.objects.get_or_create(user=instance)
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
