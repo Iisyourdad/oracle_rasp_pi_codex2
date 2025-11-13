d316ebaed1944e0415de8aefecb382f34f6cd029
Tyler <tyler.westbrook1@gmail.com>
2025-11-13 14:00:17 -0600

    Redriect for wifi website

--- raspberry_pi/django_server_run.sh
+++ raspberry_pi/django_server_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd ~/Downloads/final_rasp_pi_recipe || exit
+cd ~/Downloads/oracle_rasp_pi_codex2/raspberry_pi || exit
 source venv/bin/activate
 python manage.py runserver 0.0.0.0:8000
